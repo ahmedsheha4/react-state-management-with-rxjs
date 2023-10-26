e1d187d71c8a8bcec2b81fdfe9d5234c86f94398
unknown <ahmed.sheha097@gmail.com>
2023-10-26 07:49:22 +0300

    test

--- src/pages/main.js
+++ src/pages/main.js
@@ -6,5 +6,3 @@ export default function Main() {
   }, []);
   return <div>Main</div>;
 }
-// we have a service that does setInterval, and when interval is equal to 10 fire a notification
-// inside of  the main screen, when a notification is fired we want to react by showing an alert.
